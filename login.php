d0fb1e648c15cb965147532297c1f10095c38ed3
Ashen Shanuka <99609879+ashenshanu@users.noreply.github.com>
2024-01-28 11:53:57 +0530

    as

--- login.php
+++ login.php
@@ -12,7 +12,7 @@
 <body>
   <div class="wrapper">
     <section class="form login">
-      <header>Realtime Chat App</header>
+      <header>ChatMe</header>
       <form action="#" method="POST" enctype="multipart/form-data" autocomplete="off">
         <div class="error-text"></div>
         <div class="field input">
